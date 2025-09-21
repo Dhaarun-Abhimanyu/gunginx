2e382f916e76cb85593b40e5aa2082af33f034f7
Dhaarun <dhaarunabhimanyu@gmail.com>
2025-09-21 22:47:06 +0530

    created sample http server

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+
+	fmt.Println("Server running at http://localhost:" + port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Println("Server failed to start:", err)
+	}
+
 }
